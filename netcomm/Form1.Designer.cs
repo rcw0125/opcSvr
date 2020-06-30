c6e943ada385dd4d2f720b17e3f5ff3601a5ef5b
Administrator <Administrator@DESKTOP-F89DO8O>
2020-06-30 17:54:47 +0800

    添加netcomm网络通讯检测模块程序

---
+++ netcomm/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace netcomm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ipDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.onlineDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.offlineDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ipinfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ipinfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(725, 630);
+            this.splitContainer1.SplitterDistance = 60;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(502, 25);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(559, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始检测";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ipDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.onlineDataGridViewTextBoxColumn,
+            this.offlineDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ipinfoBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 566);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            // 
+            // ipDataGridViewTextBoxColumn
+            // 
+            this.ipDataGridViewTextBoxColumn.DataPropertyName = "ip";
+            this.ipDataGridViewTextBoxColumn.HeaderText = "ip地址";
+            this.ipDataGridViewTextBoxColumn.Name = "ipDataGridViewTextBoxColumn";
+            this.ipDataGridViewTextBoxColumn.ReadOnly = true;
+            this.ipDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "用途";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 300;
+            // 
+            // onlineDataGridViewTextBoxColumn
+            // 
+            this.onlineDataGridViewTextBoxColumn.DataPropertyName = "online";
+            this.onlineDataGridViewTextBoxColumn.HeaderText = "成功次数";
+            this.onlineDataGridViewTextBoxColumn.Name = "onlineDataGridViewTextBoxColumn";
+            this.onlineDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // offlineDataGridViewTextBoxColumn
+            // 
+            this.offlineDataGridViewTextBoxColumn.DataPropertyName = "offline";
+            this.offlineDataGridViewTextBoxColumn.HeaderText = "失败次数";
+            this.offlineDataGridViewTextBoxColumn.Name = "offlineDataGridViewTextBoxColumn";
+            this.offlineDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // ipinfoBindingSource
+            // 
+            this.ipinfoBindingSource.DataSource = typeof(netcomm.ipinfo);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 600000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 630);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "网络通讯检测";
+          
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ipinfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.BindingSource ipinfoBindingSource;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ipDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn onlineDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn offlineDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ netcomm/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace netcomm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int hour = 0;     
+        public IpData ipdata = new IpData();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setupIpList();
+            dataGridView1.DataSource = ipdata;
+        }
+        public DateTime lasttime=DateTime.Now;
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var item in ipdata)
+            {
+                item.online = 0;
+                item.offline = 0;
+            }
+            //后台工作开始进行
+            backgroundWorker1.RunWorkerAsync();
+        }
+        #region timer:每小时上传一次检测结果
+        public int commcount = 1;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+
+            if (DateTime.Now.Hour != hour)
+            {
+                hour = DateTime.Now.Hour;
+                commIPlist();
+            }
+            timer2.Enabled = true;
+        }
+        public void commIPlist()
+        {
+            string pre = DateTime.Now.ToString("MMdd");
+            Int32 curcount = Convert.ToInt32(pre + "00000");
+            curcount += commcount;
+            foreach (var item in ipdata)
+            {
+                comm(item.ip, item.name, curcount);
+            }
+        }
+        /// <summary>
+        /// ping IP地址 写入到数据库
+        /// </summary>
+        /// <param name="ip"></param>
+        public void comm(string ip, string name, Int32 count = 0)
+        {
+
+            try
+            {
+                int online = 0, offline = 0;
+                Ping ping = new Ping();
+                for (int i = 0; i < 6; i++)
+                {
+                    PingReply pingReply = ping.Send(ip);
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        online++;
+                    }
+                    else
+                    {
+                        offline++;
+                    }
+                }
+                if (offline > 0)
+                {
+                    var db = new DbHelp("192.168.36.162", "cmsdb", "sa", "xgmes");
+                    string sql = "insert into a_ip_comm(ip,online,offline,count,name) values('" + ip + "'," + online + "," + offline + "," + count + ",'" + name + "')";
+                    db.ExeSql(sql);
+                    commcount++;
+                }
+
+            }
+            catch
+            {
+
+            }
+
+        }
+        #endregion
+
+        #region ip地址列表初始化
+        public void setupIpList()
+        {
+            addIpInfo("192.168.36.151", "MES服务器，数据库备用", 1);
+            addIpInfo("192.168.36.153", "MES服务器，数据库在线", 2);
+            addIpInfo("192.168.36.155", "MES服务器，应用备用", 3);
+            addIpInfo("192.168.36.157", "MES服务器，应用在线", 4);
+            addIpInfo("192.168.36.159", "MES服务器，实时数据库", 5);
+            addIpInfo("192.168.36.169", "MES服务器，数据采集工程站", 6);
+            addIpInfo("192.168.36.167", "MES服务器，数据采集机", 7);
+            addIpInfo("192.168.36.168", "MES服务器，数据采集机", 8);
+            addIpInfo("192.168.48.201", "服务器1，群集1：内网等应用", 9);
+            addIpInfo("192.168.48.202", "服务器2，群集1：内网等应用", 10);
+            addIpInfo("192.168.48.203", "服务器3，群集1：内网等应用", 11);
+            addIpInfo("192.168.48.204", "服务器4，群集2：WinnCC等生产应用", 12);
+            addIpInfo("192.168.48.205", "服务器5，群集2：WinnCC等生产应用", 13);
+            addIpInfo("192.168.48.206", "服务器6，群集2：WinnCC等生产应用", 14);
+            addIpInfo("192.168.48.207", "服务器7，群集3：MES通讯", 15);
+            addIpInfo("192.168.48.208", "服务器8，群集3：MES通讯", 16);
+            addIpInfo("192.168.48.209", "服务器9，虚拟机：群集管理器", 17);
+            addIpInfo("192.168.48.200", "服务器10，群集4：群集管理器", 18);
+            addIpInfo("192.168.48.22", "设备科计算机：数据库备份", 19);
+            addIpInfo("192.168.48.230", "虚拟机：数据上传", 20);
+            addIpInfo("192.168.48.232", "虚拟机：数据上传", 21);
+            addIpInfo("192.168.48.239", "虚拟机：AOD数据上传", 22);
+            addIpInfo("192.168.48.243", "虚拟机：3、4#机大包重量下传", 23);
+
+        }
+        public void addIpInfo(string ip, string name, int idx)
+        {
+            ipinfo curipinfo = new ipinfo();
+            curipinfo.ip = ip;
+            curipinfo.name = name;
+            curipinfo.idx = idx;
+            //ipList.Add(curipinfo);
+            ipdata.Add(curipinfo);
+        }
+
+        #endregion
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = sender as BackgroundWorker;
+
+            bw.ReportProgress(1, "开始检测IP地址");
+            Ping ping = new Ping();
+            int count = ipdata.Count;
+            if (count < 1)
+            {
+                count = 20;
+            }
+            foreach (var item in ipdata)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    PingReply pingReply = ping.Send(item.ip);
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        item.online++;
+                    }
+                    else
+                    {
+                        item.offline++;
+                    }
+                }
+                int process = 1 + Convert.ToInt32(item.idx * 99 / count);
+                bw.ReportProgress(process, item.name+"检测完毕");
+            }
+            bw.ReportProgress(100, "检测完毕");
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 3)
+                {
+                    if (e.Value != null && Convert.ToDouble(e.Value) > 0)
+                    {
+                        e.CellStyle.BackColor = Color.Red;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    public class IpData : BindingList<ipinfo>
+    {     
+        public void Refresh()
+        {
+            ListChangedEventArgs arg = new ListChangedEventArgs(ListChangedType.Reset, -1);
+            this.OnListChanged(arg);
+        }
+    }
+    public class ipinfo: INotifyPropertyChanged
+    { 
+        public int idx { get; set; }
+        public string ip { get; set; }
+        private int _online = 0;
+        public int online
+        {
+            get
+            {
+                return _online;
+            }
+            set
+            {
+                _online = value;
+                RaisePropertyChanged("online");
+
+            }
+        }
+        private int _offline = 0;
+        public int offline
+        {
+            get
+            {
+                return _offline;
+            }
+            set
+            {
+                _offline = value;
+                RaisePropertyChanged("offline");
+
+            }
+        }
+        public string name { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+    }
+}
